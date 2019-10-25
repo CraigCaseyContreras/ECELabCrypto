a8d15e36efcaeb528786d16e7b5731131a0c3fde
CraigCaseyContreras <craigcc96@outlook.com>
2019-10-25 09:37:52 -0400

    finished getting public key from certificate

--- CryptoLab3/user1/encryptionUSER1.py
+++ CryptoLab3/user1/encryptionUSER1.py
@@ -34,8 +34,11 @@ def gen_iv():
 	return os.urandom(16)
 
 def get_key_used_in_encryption(data):
-	key_used = data[:16]
-	return key_used
+	return data[:16]
+
+def split(strng, sep, pos):
+	strng = strng.split(sep)
+	return sep.join(strng[:pos]), sep.join(strng[pos:])
 
 def main():
 	#Create an encrypted file with a randomly generated secret key
@@ -57,10 +60,33 @@ def main():
 	#First, need to get the secret key used in the encryption
 	key = get_key_used_in_encryption(ciphertext)
 	
+	
 	#Now need to get the public key of user2 - From User 2 certificate in keystore k1
-	with open('../k1.pem', 'r') as file:
-		reader = file.readlines()
-
+	
+	#The first section is the key key_k1, then the cert1, then the cert2 in keystore k1
+	#The first section is the key key_k2, then the cert2, then the cert1 in keystore k2
+	
+	#Reads content of keystore k1
+	with open('../k1.pem', 'r') as infile:
+		reader = infile.read()
+	
+	#Gets the certificate for user2
+	strng = reader
+	lister = split(strng, '-----BEGIN CERTIFICATE-----', 2)
+	
+	#Writes what was received to a pem file for user2 certificate
+	with open('user2cert.pem', 'w') as file:
+		file.write("-----BEGIN CERTIFICATE-----" + lister[1])
+	
+	#Load the certificate for User 2
+	with open('user2cert.pem', 'rb') as file:
+		certificate = x509.load_pem_x509_certificate(data=file.read(), backend=default_backend())
+	
+	#Get public key of user2 from certificate
+	public_key_user2 = certificate.public_key()
+	
+	#Encrypt the secret key used for the file encryption with the public key of user 2
+	#in class as if he wants us to generate secrey key for encryption using os.urandom or if it has to be RSA generated keys?? Like in certificates.py
 	
 if __name__ == "__main__":
 	main()
